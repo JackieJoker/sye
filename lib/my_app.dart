7c820bae5fa4ab36459f41907774fb5d36030206
JackieJoker <danilocastiglia@hotmail.it>
2021-12-21 12:20:40 +0100

    added firebase support to the project

---
+++ lib/my_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Split Your Expenses',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
